4e4c32238fae9275a92f8c4f1849cabd9b8d0613
ghulamakbar14 <ghulamakbar14@gmail.com>
2017-11-30 06:12:00 +0000

    migration created

---
+++ database/migrations/2017_11_30_055814_create_films.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->date('realease_date');
+            $table->integer('rating');
+            $table->double('ticket_price');
+            $table->string('country');
+            $table->string('genre');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('films');
+    }
+}
